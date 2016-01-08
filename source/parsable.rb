2e9c70c4ffa94d429bfc7e7b8fb636c77b576130
Dan Andersen & Joe Plonsker <pair+dandersen2.plonsker@devbootcamp.com>
2016-01-08 15:10:03 -0500

    Add deck class and parsable module for creating deck object full of cards from txt file

--- source/parsable.rb
+++ source/parsable.rb
@@ -1,11 +1,34 @@
 require 'pry'
+require_relative 'card'
 
 module Parsable
 
-  def txt_parser #should parse the file into a bunch of card objects
-
+  def txt_parser(input_file) #should parse the file into a bunch of card objects
+    definitions = []
+    terms = []
+    File.open(input_file, "r") do |file|
+      file.readlines.each_with_index do |line, idx|
+        if (idx) % 3 == 0
+          definitions << line.chomp
+        elsif (idx-1) % 3 == 0
+          terms << line.chomp
+        end
+      end
+    end
+    words_array = terms.zip(definitions)
+    # words_array.map.with_index do |word_sub_array, index|
+    #   Card.new({:word => word_sub_array[0], :definition => word_sub_array[1], :card_id => index })
+      # p word_sub_array
+      # p index
+    # end
+    # puts words_array
+    # words_array.each do |word|
+    #   puts word
+    # end
   end
 
+# p txt_parser("flashcard_samples.txt")
+# p terms.zip(definitions)
 
 end
 
